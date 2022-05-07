d9e14a99b1718e497e08bc6e3ddab971324e093b
MarshallNakamura <Mnakamura@hawk.iit.edu>
2022-05-07 10:54:19 -0500

    Initial Commit

---
+++ Cars project github!!/Cars_project_github__App.swift
@@ -0,0 +1,17 @@
+//
+//  Cars_project_github__App.swift
+//  Cars project github!!
+//
+//  Created by admin on 5/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Cars_project_github__App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
