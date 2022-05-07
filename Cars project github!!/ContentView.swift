3dd76fb8863137446d58acee6dc2f1ce4151e926
MarshallNakamura <Mnakamura@hawk.iit.edu>
2022-05-07 11:01:11 -0500

    Friendly message for my teammate to help with morale.

--- Cars project github!!/ContentView.swift
+++ Cars project github!!/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("My teammate is really well organized!")
             .padding()
     }
 }
