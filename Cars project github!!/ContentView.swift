e421afaa8a3e8433a52e28a880ef74879c4dad6d
Yazmin Romo <yromo@cps.edu>
2022-05-08 20:59:12 -0500

    Added the enums on the main

--- Cars project github!!/ContentView.swift
+++ Cars project github!!/ContentView.swift
@@ -6,6 +6,41 @@
 //
 
 import SwiftUI
+enum Doors: String {
+    case two = "2"
+    case four = "4"
+}
+enum Brand: String {
+        case toyota
+        case ferrari
+        case honda
+        case dodge
+        case chevy
+}
+
+enum Size: String {
+        case big
+        case small
+}
+
+enum Terrain: String {
+        case automatic
+        case manual
+        case both
+}
+
+enum Energy: String {
+        case gas
+        case electric
+        case hybrid
+}
+
+enum Drive: String {
+    case awd
+    case fwd
+    case fourByFour = "4X4"
+}
+
 
 struct ContentView: View {
     var body: some View {
